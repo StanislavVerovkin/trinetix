f42e986fd22c402e5d4ec3eb0399a2bd4b8f5728
Stas Verovkin <stas@PW-MB-PRO-02.local>
2020-03-05 14:45:12 +0200

    320px

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: [ './header.component.scss' ]
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  resize = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: any) {
+    if (event && event.target.innerWidth <= 800) {
+      this.resize = true;
+    } else if (window.innerWidth <= 800) {
+      this.resize = true;
+    } else {
+      this.resize = false;
+    }
+  }
 
   constructor() {
   }
 
   ngOnInit() {
+    this.onResize();
   }
-
 }
